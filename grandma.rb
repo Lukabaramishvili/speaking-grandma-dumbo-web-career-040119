d3e50896de6fda713c0fa151c4300895ed02d973
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-15 19:34:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string != string.upaces
+  if string != string.upaces()
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
